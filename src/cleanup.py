dfc88520558606a22b667cf853f442afe671b6b1
Aymeric Flaisler <aflaisler@gmail.com>
2017-02-10 13:38:27 -0800

    data pipeline without missing data (no imputation yet

--- src/cleanup.py
+++ src/cleanup.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from zipfile import ZipFile
 import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.cross_validation import train_test_split, cross_val_score
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.linear_model import LinearRegression
 
 
 def load_data(filename):
@@ -11,7 +15,7 @@ def load_data(filename):
         ModelID
         datasource
         YearMade
-        MachineHoursCurrentMeter
+        c
         Saleprice
         fiBaseModel
         fiSecondaryDesc
@@ -27,13 +31,35 @@ def load_data(filename):
     '''
     zf = ZipFile(filename + '.zip')
     df = pd.read_csv(filename + '.csv')
+    # selecting specific features we will work with
+    keep_cols = ["MachineID", "ModelID", "datasource", "YearMade", "MachineHoursCurrentMeter", "SalePrice",
+                 "fiBaseModel", "fiSecondaryDesc", "state", "ProductGroup", "Enclosure", "Ride_Control", "Stick", "Hydraulics"]
+    df = df[keep_cols]
     # removing weird year - will focus on those later on if we have time
-    df = df.ix[df['YearMade'] > 1900].shape
-    year = year[year != 1000]
+    df = df.ix[df['YearMade'] > 1900]
+    # save df with missing data
 
-    price_v_year = df[['SalePrice', 'YearMade']]
+    return df
+    #, df_year_missing, df_machineHour_missing
+
+
+def missing_data_imputation(df_train, df_with_missing_data, column_to_fill):
+    '''
+    - input: pandas dataframe
+    - output: pandas dataframe
+    - how it works: fill the df with missing data using RF trained on the df with
+    data
+    '''
+    y = np.array(df_train.pop('MachineHoursCurrentMeter').values)
+    X = np.array(df_train.values)
+
+    # train a random forest to fill hours
+    md = RandomForestRegressor()
+    md.fit(X, y)
+    return None
 
 
 if __main__ == '__name__':
     # Don't add the extension to the filename as it will extract zip to csv
     filename = '../data/Train'
+    df = load_data(filename)
